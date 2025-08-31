9eeeb68b35163b9fdfcf524f4880f396b11808d6
Virendra <shakyaraju605@gmail.com>
2025-08-31 18:13:27 +0530

    Added Thumbnails

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // script.js
-// Simple smooth scroll for nav links
+// Smooth scroll only for section links starting with "#"
 document.querySelectorAll("nav a").forEach(link => {
   link.addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector(link.getAttribute("href"))
-            .scrollIntoView({ behavior: "smooth" });
+    const href = link.getAttribute("href");
+
+    if (href.startsWith("#")) {
+      e.preventDefault();
+      document.querySelector(href).scrollIntoView({ behavior: "smooth" });
+    }
+    // If it's not "#", let it open normally (like thumbnails.html)
   });
 });
